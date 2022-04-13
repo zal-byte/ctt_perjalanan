0019d1d667a06ceac409f64cb5c2009a5db2a838
zal-byte <rizal.solehudin45@gmail.com>
2022-04-13 16:07:15 +0700

    modal

--- resources/views/auth_lay/signup.blade.php
+++ resources/views/auth_lay/signup.blade.php
@@ -11,18 +11,25 @@
 					Catatan Perjalanan | Daftar
 				</h3>
 				<form id="s_form">
-					<input type='text' id='username' placeholder="Username" required class="form-control" aria-describedby="validateUsernameFeedback">
-					<div id="validateUsernameFeedback" class="invalid-feedback">
-						<p id="usr_msg">
-
-						</p>
+					<input type='text' id='username' placeholder="Username" required class="form-control">
+					<div id="e-1" class="bg-danger text-white rounded" style="margin-top: 5px;display: none;padding-left: 10px; padding-top: 10px; padding-bottom: 10px;">
+						<i id="usr_msg"></i>
 					</div>
 					<br>
 					<input type='text' id="name" placeholder="Full name" required class="form-control">
+					<div id="e-2" class="bg-danger text-white rounded" style="margin-top: 5px;display: none;padding-left: 10px; padding-top: 10px; padding-bottom: 10px;">
+						<i id="name_msg"></i>
+					</div>
 					<br>
 					<input type='password' id='password' placeholder="Password" required class="form-control">
+					<div id="e-3" class="bg-danger text-white rounded" style="margin-top: 5px;display: none;padding-left: 10px; padding-top: 10px; padding-bottom: 10px;">
+						<i id="pass_msg"></i>
+					</div>
 					<br>
 					<input type="password" id="verify_password" placeholder="Verify password" required class="form-control">
+					<div id="e-4" class="bg-danger text-white rounded" style="margin-top: 5px;display: none;padding-left: 10px; padding-top: 10px; padding-bottom: 10px;">
+						<i id="pw_msg"></i>
+					</div>
 					<br>
 					<div class="justify-content-center align-items-center">
 						<button id='s_submit' class="btn btn-solid text-white bg-success" style="width:100%;">
@@ -40,14 +47,14 @@
 			</div>			
 		</div>
 	</div>
-	<div id="s_modal_success" class="modal fade" role="dialog" tabindex="-1">
+	<div id="s_modal" class="modal fade" role="dialog" tabindex="-1">
 		<div class="modal-dialog">
 			<div class="modal-content">
 				<div class="modal-header">
 					<div class="justify-content-center d-flex">
-						<span class="fa-solid fa-circle-check fa-2xl"></span>
+						<span class="fa-solid fa-circle-check fa-2xl" id='ic-res'></span>
 					</div>
-					<button type="button" class="fa-solid fa-xmark" onclick="$('#s_modal_success').modal('hide');"></button>
+					<button type="button" class="fa-solid fa-xmark" onclick="$('#s_modal').modal('hide');"></button>
 				</div>
 				<div class="modal-body">
 					<p id="msg">
@@ -55,10 +62,10 @@
 					</p>
 				</div>
 				<div class="modal-footer">
-					<a href="{{route('login')}}" style="text-decoration:none;" class="btn btn-solid btn-success text-white">
+					<a href="{{route('login')}}" id='log-i' style="text-decoration:none;" class="btn btn-solid btn-success text-white">
 						Login
 					</a>
-					<a onclick="$('#s_modal_success').modal('hide');" style="text-decoration:none;" class="btn btn-solid btn-warning text-white">
+					<a onclick="$('#s_modal').modal('hide');" style="text-decoration:none;" class="btn btn-solid btn-warning text-white">
 						Close
 					</a>
 				</div>
@@ -103,8 +110,14 @@
 											$("#password").val("");
 											$("#verify_password").val("");
 											$("#name").val("");
-											$("#s_modal_success").modal('show');
+											$("#s_modal").modal('show');
 											$("#msg").html(js['msg']);
+										}else{
+											$("#log-i").css({"display":"block"});
+											$("#s_modal").modal('show');
+											$("#msg").html(js['msg']);
+											$("#ic-res").toggleCss('fa-xmark fa-circle-exclamation');
+
 										}
 									}
 								});
@@ -113,15 +126,19 @@
 
 							}else{
 								//password doesn't match
+								$("#pw_msg").css({"display":"block"});
+								$("#pw_msg")
 								$("#verify_password").addClass("is-invalid");
 							}
 						}else{
 							alert("Password length more than 5");	
 						}
 					}else{
-						alert("Please fill naem field");
+						$("#e-2").css({"display":"block"});
+						$("#name_msg").html("Please fill the name field");
 					}
 				}else{
+					$("#e-1").css({"display":"block"});
 					$("#usr_msg").html("Don't leave it blank");
 				}
 			});
